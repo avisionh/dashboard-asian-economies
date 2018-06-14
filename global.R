6571b52d675f1acf4b5fa2653e7ac9431e8b7afa
A Ho <avision.ho@education.gov.uk>
2018-06-14 21:59:42 +0100

    Data/asia development bank (#1)
    * chore: add further data from ADB website
    
    * chore: update README.md with new data
    
    * chore: add travis.yml file for continuous deployment
    
    * chore: simplify .travis.yml file to get successful build
    
    * chore: add .Rbuildignore file for travis
    
    * chore: add Rproject and starter scripts for app
    
    * chore: update .travis.yml to get build to work
    
    * chore: add packrat to project
    
    * chore: update .travis.yml for shiny app and packrat
    
    * refactor: add information to app
    
    * chore: remove white space in .travis.yml
    
    * chore: take packrat::snapshot
    
    * chore: edit .travis.yml to try and get build to work

---
+++ global.R
@@ -0,0 +1,25 @@
+# -------- #
+# global.R #
+# -------- #
+
+# DESC: global.R script used to create static objects that app needs to run.
+#       Includes loading and manipulating daya, and defining functions to be used in server.R
+
+# Install these packages before running the app:
+# install.packages(c("shiny", "shinydashboard", "DT", "RODBC", "tidyverse", "scales"))
+# install.packages("shinyBS")
+
+
+# Packages ------------------------------------------------------------
+
+# shiny app development and appearance
+library(shiny)
+library(shinydashboard)
+
+
+
+# Data Import -------------------------------------------------------------
+
+# data_exchangerates <- readr::read_csv()
+# data_externaldebtoutstanding <- readr::read_csv()
+
